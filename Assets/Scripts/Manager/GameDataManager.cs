377e19ff4d85565024544fff5e1d26be05868cf3
liknlie7 <liknlie@gmail.com>
2020-11-09 17:35:29 +0900

    Working...

---
+++ Assets/Scripts/Manager/GameDataManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDataManager : MonoBehaviour
+{
+    // トータルスコア
+    private static int totalScore = 0;
+    public static int TotalScore
+    {
+        get { return totalScore; }
+        set { totalScore = value; }
+    }
+
+    // 良判定スコア
+    [SerializeField]
+    int greatScore;
+    public int GreateScore
+    {
+        get { return greatScore; }
+    }
+
+    // 可判定スコア
+    [SerializeField]
+    int goodScore;
+    public int GoodScore
+    {
+        get { return goodScore; }
+    }
+
+    // ミス判定スコア
+    [SerializeField]
+    int missScore;
+    public int MissScore
+    {
+        get { return missScore; }
+    }
+
+    // 敵の総撃破数
+    private static int killedEnemyNum;
+    public static int KilledEnemyNum
+    {
+        get { return killedEnemyNum; }
+        set { killedEnemyNum = value; }
+    }
+
+    // 良判定の数
+    private static int greatDecisionNum;
+    public static int GreatDecisionNum
+    {
+        get { return greatDecisionNum; }
+        set { greatDecisionNum = value; }
+    }
+
+    // 可判定の数
+    private static int goodDecisionNum;
+    public static int GoodDecisionNum
+    {
+        get { return goodDecisionNum; }
+        set { goodDecisionNum = value; }
+    }
+
+    // ミス判定の数
+    private static int missDecisionNum;
+    public static int MissDecisionNum
+    {
+        get { return missDecisionNum; }
+        set { missDecisionNum = value; }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ScoreTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTest : MonoBehaviour
+{
+    // Managerゲームオブジェクト
+    [SerializeField]
+    GameObject manager;
+
+    // Managerスクリプト格納用
+    GameDataManager gameDataManager; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Managerスクリプト取得
+        gameDataManager = manager.GetComponent<GameDataManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Xキー押下
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+
+        }
+        // Yキー押下
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+
+        }
+        // Zキー押下
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+        }
+
+    }
+}

c822a609fc86061016345a6eef6fc186dc010ead
DenoBox <yosinozukinoanimeotaku@gmail.com>
2020-11-13 15:44:27 +0900

    Re Create PlayerMove
    PlayerMoveを再作成
    テスト用ステージを作成

---
+++ Assets/Scripts/Utility/CalculationUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GhosterUtility
+{
+    // 計算系のUtility
+    public static class CalculationUtility
+    {
+        // 指定範囲内に値が収まっているか
+        // true : 範囲内 /　false : 範囲外
+        public static bool IsWithinRange(float _value,float _min, float _max)
+        {
+            if (_min <= _value &&
+                _value <= _max)
+                return true;
+
+            return false;
+        }
+    }
+}
+

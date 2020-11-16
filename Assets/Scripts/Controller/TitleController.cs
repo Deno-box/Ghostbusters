592d2456c749bdf03cbd9d769c9ce2ebf65f71d9
liknlie7 <liknlie@gmail.com>
2020-11-16 16:23:27 +0900

    title scene complete

--- Assets/Scripts/Controller/TitleController.cs
+++ Assets/Scripts/Controller/TitleController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TitleController : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class TitleController : MonoBehaviour
         ALL_TYPE = 3,
     }
 
-    // イメージ補正X値
+    // 画像位置補正X値
     private const float IMAGE_OFFSET_X = 145.0f;
 
     // テキスト
@@ -24,8 +25,9 @@ public class TitleController : MonoBehaviour
     Text[] texts = new Text[(int)TextType.ALL_TYPE];
 
     // 矢印画像
+    [NamedArrayAttribute(new string[] { "Left Arrow", "Right Arrow" })]
     [SerializeField]
-    Image arrowImage = null;
+    Image[] arrowImage = new Image[2];
 
     // 選択中
     private int selecting = (int)TextType.NEW_GAME;
@@ -34,22 +36,70 @@ public class TitleController : MonoBehaviour
     void Start()
     {
         // 初期座標格納用
-        RectTransform tmp = texts[(int)TextType.NEW_GAME].rectTransform;
+        RectTransform temp = this.texts[(int)TextType.NEW_GAME].rectTransform;
 
         // 矢印画像初期座標設定
-        arrowImage.rectTransform.position = new Vector3(tmp.position.x - IMAGE_OFFSET_X, tmp.position.y, tmp.position.z);
+        this.arrowImage[0].rectTransform.position = new Vector3(temp.position.x + IMAGE_OFFSET_X, temp.position.y, temp.position.z);
+        this.arrowImage[1].rectTransform.position = new Vector3(temp.position.x - IMAGE_OFFSET_X, temp.position.y, temp.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.RightArrow))
+        // ←キー（Aキー）を押下
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.A))
         {
-
+            if (this.selecting < (int)TextType.ALL_TYPE - 1)
+            {
+                this.selecting++;
+            }
+            ChangeArrowPos(this.selecting);
         }
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        // →キー（Dキー）を押下
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.D))
         {
+            if (this.selecting > 0)
+            {
+                this.selecting--;
+            }
+            ChangeArrowPos(this.selecting);
+        }
 
+        // モード選択
+        ChoiceMode();
+    }
+
+    // テキストの変更
+    void ChangeArrowPos(int _selecting)
+    {
+        // 選択中のテキスト位置を取得
+        RectTransform temp = this.texts[_selecting].rectTransform;
+
+        // 画像座標設定
+        this.arrowImage[0].rectTransform.position = new Vector3(temp.position.x + IMAGE_OFFSET_X, temp.position.y, temp.position.z);
+        this.arrowImage[1].rectTransform.position = new Vector3(temp.position.x - IMAGE_OFFSET_X, temp.position.y, temp.position.z);
+    }
+
+    // モード選択
+    void ChoiceMode()
+    {
+        //　Enterキー（SPACEキー）を押下
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (this.selecting)
+            {
+                // ゲームスタート
+                case (int)TextType.NEW_GAME:
+                    // プレイシーンへ遷移
+                    SceneManager.LoadScene(1);
+                    break;
+                // 続きから
+                case (int)TextType.CONTINUE:
+                    break;
+                // オプション
+                case (int)TextType.OPTIONS:
+                    break;
+            }
         }
     }
 }
